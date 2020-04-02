3442ecae38f0307462d5a138286c784718b781b4
yijin.zhou@zhangmen.com <yijin.zhou@zhangmen.com>
2020-04-02 22:26:40 +0800

    *)修复demo点击奔溃bug *)路由demo增加功能展示 *)完善服务注册耗时测试

---
+++ HXRouterDemo/Community/CommunityModuleHeader.h
@@ -0,0 +1,20 @@
+//
+//  CommunityModuleHeader.h
+//  HXRouterDemo
+//
+//  Created by James on 2020/4/2.
+//  Copyright © 2020 James. All rights reserved.
+//
+
+#ifndef CommunityModuleHeader_h
+#define CommunityModuleHeader_h
+
+#import "HXRouter.h"
+
+static NSString *HXRouterNamespace_Community = @"communitynamespace";
+
+static NSString *URLString_Community_Home = @"community://home";
+static NSString *URLString_Community_List = @"community://list";
+
+
+#endif /* CommunityModuleHeader_h */
